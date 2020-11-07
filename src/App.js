39eb12d1059cf1ef35f860d85a39c5369715cae1
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2020-11-07 23:06:17 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import { useEffect, useState } from 'react';
+
 
 function App() {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      let result;
+      await axios.get('http://localhost:8000/api/')
+                 .then(res => {
+                   result = res.data;
+                 })
+                 .catch(err => {
+                   console.log(err);
+                 })
+
+      // [...配列名]でディープコピーすることができる
+      let newBooks = [...books];
+      newBooks = newBooks.concat(result);
+      setBooks(newBooks);
+    }
+
+    fetchData();
+  }, []);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      { books.map(item => (
+        <div key={item.id}>
+          <h1>{item.title}</h1>
+          <p>{item.author}</p>
+        </div>
+      ))}
     </div>
   );
 }
